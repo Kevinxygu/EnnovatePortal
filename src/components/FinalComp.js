d67a28f8c8f711b347e0977dba853241a6b77ff8
KevinGooby <87846274+KevinGooby@users.noreply.github.com>
2023-01-19 23:18:57 -0800

    Edited portal page for final competition

--- src/components/FinalComp.js
+++ src/components/FinalComp.js
@@ -96,12 +96,7 @@ class FinalComp extends React.Component {
     </div>
         )
 
-        const livePage = (
-            <div class="final-comp">
-            <h2 class="section-title">Final Competition</h2>
-            <div class="section-line"></div>
-                <div class="content">
-                <div class="body">
+        const submission = (
             <div id="file-submission">
                 <div id ="h1desc">
                     <h1 >Please RSVP with this link, then submit your business plan and final competition slides below:<br id = "h1desc" />
@@ -146,96 +141,109 @@ class FinalComp extends React.Component {
                     </fieldset>
                 </form>
             </div>
+        )
+
+        const panelists = (
+            <div class="panelists">
+
+                <div class="penlist">
+                    <img class = "judgeimg" src = "FinalCompRubric.png"></img>
+                    <p class= "judgename">Big Guy</p>
+                </div>
+                <div class="penlist">
+                    <img class = "judgeimg" src = "FinalCompRubric.png"></img>
+                    <p class= "judgename">Big Guy</p>
+                </div>
+                <div class="penlist">
+                    <img class = "judgeimg" src = "FinalCompRubric.png"></img>
+                    <p class= "judgename">Big Guy</p>
+                </div>
+                <div class="penlist">
+                    <img class = "judgeimg" src = "FinalCompRubric.png"></img>
+                    <p class= "judgename">Big Guy</p>
+                </div>
+
+
+            </div>
+        )
+
+        const livePage = (
+            <div class="final-comp">
+            <h2 class="section-title">Final Competition</h2>
+            <div class="section-line"></div>
+                <div class="content">
+                <div class="body">
+            
    
+                <h1 id="itinerarytitle">Itinerary</h1>
+                    <div id="itinerary">
+                    <div id="schedule-timeline">
+                    <div id="bluebar"></div>
+                    <div id = "dots">
+                    <div class="outline" id="two"><div class="dot" id="activity1"></div></div>
+                    <div class="outline" id="three"><div class="dot" id="activity2"></div></div>
+                    <div class="outline" id="four"><div class="dot" id="activity3"></div></div>
+                    <div class="outline" id="five"><div class="dot" id="activity4"></div></div>
+                    <div class="outline" id="six"><div class="dot" id="activity5"></div></div>
+                    <div class="outline" id="seven"><div class="dot" id="activity6"></div></div>
+                    </div>
+                    </div>
 
-<div id="itinerary">
-<h1 id="itinerarytitle">Itinerary</h1>
-<div id="schedule-timeline">
-<div id="bluebar"></div>
-<div id = "dots">
-<div class="outline" id="two"><div class="dot" id="activity1"></div></div>
-<div class="outline" id="three"><div class="dot" id="activity2"></div></div>
-<div class="outline" id="four"><div class="dot" id="activity3"></div></div>
-<div class="outline" id="five"><div class="dot" id="activity4"></div></div>
-<div class="outline" id="six"><div class="dot" id="activity5"></div></div>
-<div class="outline" id="seven"><div class="dot" id="activity6"></div></div>
-</div>
-</div>
+                    <div class="schedule-description">
+                    <div class="description-item"><h1>12:45 PM - 1:00PM</h1><p class="description">Registration</p></div>
+                    <div class="description-item"><h1>1:00 PM - 1:30 PM</h1><p class="description">Opening Ceremony</p></div>
+                    <div class="description-item"><h1>1:30 PM - 3:00 PM</h1><p class="description">League Presentations</p></div>
+                    <div class="description-item"><h1>3:00 PM - 3:30 PM</h1><p class="description">Judge Deliberation + Kahoot!</p></div>
+                    <div class="description-item"><h1>3:30 PM - 4:45 PM</h1><p class="description">Finalist Presentation</p></div>
+                    <div class="description-item"><h1>4:45 PM - 5:00 PM</h1><p class="description">Closing Celebration</p></div>
 
-<div class="schedule-description">
-<div class="description-item"><h1>12:45 PM - 1:00PM</h1><p class="description">Registration</p></div>
-<div class="description-item"><h1>1:00 PM - 1:30 PM</h1><p class="description">Opening Ceremony</p></div>
-<div class="description-item"><h1>1:30 PM - 3:00 PM</h1><p class="description">League Presentations</p></div>
-<div class="description-item"><h1>3:00 PM - 3:30 PM</h1><p class="description">Judge Deliberation + Kahoot!</p></div>
-<div class="description-item"><h1>3:30 PM - 4:45 PM</h1><p class="description">Finalist Presentation</p></div>
-<div class="description-item"><h1>4:45 PM - 5:00 PM</h1><p class="description">Closing Celebration</p></div>
+                    </div>
+                    </div>
 
-</div>
-</div>
+                    <div class="league-information">
+                    <h1 class="leagueheading">League Information</h1>
+                    <h4 id="leaguename">League 1:</h4>
+                    <ul>
+                        <li>School 1</li>
+                        <li>School 2</li>
+                    </ul>
 
-<div class="league-information">
-<h1 id = "leagueheading">League Information</h1>
-<h4 id="leaguename">League 1:</h4>
-<ul>
-    <li>School 1</li>
-    <li>School 2</li>
-</ul>
+                    <h4 id="leaguename">League 2:</h4>
+                        <ul>
+                            <li>School 1</li>
+                            <li>School 2</li>
+                        </ul>
 
-<h4 id="leaguename">League 2:</h4>
-    <ul>
-        <li>School 1</li>
-        <li>School 2</li>
-    </ul>
+                    </div>
 
-</div>
+                    <h1 class="leagueheading">Judging Rubric</h1>
+                    <div class="judging-rubric">
+                    <div id="rubriclink">
+                        <a href="https://ennovateubc.org">
+                            <img class ="rubricimg" src={FinalCompRubric}></img>
+                        </a>
+                    </div>
+                    <h1 class="leagueheading">Judging Rules</h1>
+                    <div class="rubricguidelines">
+                        <p class="judgingRules">
+                            <strong>1.</strong> All presentations will be cut off at the 7-MINUTE MARK. No additional time will be permitted.<br /><br />
 
-<div class = "judging-rubric">
-<div id="rubriclink">
-    <a href="https://ennovateubc.org">
-        <img class ="rubricimg" src={FinalCompRubric}></img>
-    </a>
-</div>
-<div class="rubricguidelines">
-    <p>
-        <strong>1.</strong> All presentations will be cut off at the 7-MINUTE MARK.<br /> No additional time will be permitted.<br /><br />
+                            <strong>2.</strong> All presentations will be followed by a 3-MINUTE Q&A period. Only judges are permitted to ask questions.<br /><br />
 
-        <strong>2.</strong> All presentations will be followed by a 3-MINUTE Q&A period.<br />Only judges are permitted to ask questions.<br />
+                            <strong>3.</strong> Please keep your cameras on and have your mics muted when you are not presenting.<br /><br />
 
-        <strong>3.</strong> Please keep your cameras on and have your mics muted when you are not presenting.<br /><br />
+                            <strong>4.</strong> Have one rep with the slides on standby, ready to screen share when it is your presentation time. Please stop screen sharing after your presentation.<br /><br />
 
-        <strong>4.</strong> Have one rep with the slides on standby, ready to screen share when it is your presentation time.<br /> Please stop screen sharing after your presentation.<br />
+                            <strong>5.</strong> Your team will receive warnings at the 3-minute, 1-minute and 10-second mark.<br /><br />
 
-        <strong>5.</strong> Your team will receive warnings at the 3-minute, 1-minute and 10-second mark.<br></br>
+                            <strong>6.</strong> 2 teams from each league will advance to the final round.<br></br>
+                        </p>
+                    </div>
+                    </div>
+                    <div class ="judge">
 
-        <strong>6.</strong> 2 teams from each league will advance to the final round.<br></br>
-    </p>
-</div>
-</div>
-<div class ="judge">
-<h1 id = "panelistheading">Panelists</h1>
-<div class="panelists">
-
-    <div class="penlist">
-        <img class = "judgeimg" src = "FinalCompRubric.png"></img>
-        <p class= "judgename">Big Guy</p>
-    </div>
-    <div class="penlist">
-        <img class = "judgeimg" src = "FinalCompRubric.png"></img>
-        <p class= "judgename">Big Guy</p>
-    </div>
-    <div class="penlist">
-        <img class = "judgeimg" src = "FinalCompRubric.png"></img>
-        <p class= "judgename">Big Guy</p>
-    </div>
-    <div class="penlist">
-        <img class = "judgeimg" src = "FinalCompRubric.png"></img>
-        <p class= "judgename">Big Guy</p>
-    </div>
-
-
-</div>
-</div>
-</div>
+                    </div>
+                    </div>
 
                 
         
@@ -243,7 +251,7 @@ class FinalComp extends React.Component {
             </div>
         )
 
-        return waitingPage;
+        return livePage;
     }
 }
 
