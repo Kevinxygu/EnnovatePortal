cf20b8b7e29f5590534d9538e17f3c8f7f285061
jpkani <jaybinpark@gmail.com>
2023-02-08 19:17:15 -0800

    Updated timeline for semester 2!

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -13,36 +13,35 @@ class Timeline extends React.Component {
 
                     <div class="program" id="sellingPeriod">
                             <div class="program-date">
-                                <h2 class="timeline-month"> Nov</h2>
-                                <h2 class="timeline-date">05</h2>
+                                <h2 class="timeline-month">Mar</h2>
+                                <h2 class="timeline-date">17</h2>
                             </div>
 
                                 <div class="program-line"></div>
                             <div class= "program-text">
-                            <h2 class="program-title">Selling Period</h2>
-                            <p class="program-desc">Selling period begins!</p>    
-    
+                            <h2 class="program-title">Idea Submission</h2>
+                            <p class="program-desc">Submit your ideas to your respective PAs!</p>    
                             </div>
                             </div>    
 
                     <div class="program" id="universitySeminar">
                             <div class="program-date">
-                                <h2 class="timeline-month"> Nov</h2>
-                                <h2 class="timeline-date">12</h2>
+                                <h2 class="timeline-month">Mar</h2>
+                                <h2 class="timeline-date">18</h2>
                             </div>
 
                                 <div class="program-line"></div>
                             <div class= "program-text">
-                            <h2 class="program-title"> University Seminar</h2>
-                            <p class="program-desc"> University seminar at UBC Sauder! Come learn tips and tricks about applying to universities</p>    
+                            <h2 class="program-title">Selling Period Starts</h2>
+                            <p class="program-desc">The start of your selling period! Feel free to start going out to sell!</p>    
     
                             </div>
                         </div>   
                         
                         <div class="program" id="sellingPeriod2">
                             <div class="program-date">
-                                <h2 class="timeline-month"> Jan</h2>
-                                <h2 class="timeline-date">09</h2>
+                                <h2 class="timeline-month">May</h2>
+                                <h2 class="timeline-date">08</h2>
                             </div>
 
                                 <div class="program-line"></div>
@@ -55,63 +54,48 @@ class Timeline extends React.Component {
 
                     <div class="program" id="rsvp">
                         <div class="program-date">
-                            <h2 class="timeline-month"> Jan</h2>
-                            <h2 class="timeline-date">18</h2>
+                            <h2 class="timeline-month">May</h2>
+                            <h2 class="timeline-date">13</h2>
                         </div>    
         
                         <div class="program-line"></div>
                         <div class= "program-text">
-                        <h2 class="program-title">Final Competition RSVP</h2>
-                        <p class="program-desc">You will need to RSVP for final competition, and we'll update that for you here when it gets closer</p>
+                        <h2 class="program-title">First Draft of Slide Deck</h2>
+                        <p class="program-desc">You will need to submit your first draft of your deck to your PAs.</p>
    
                         </div>
                         </div> 
 
                     <div class="program" id="microloan">
                         <div class="program-date">
-                            <h2 class="timeline-month"> Jan</h2>
-                            <h2 class="timeline-date">20</h2>
+                            <h2 class="timeline-month">May</h2>
+                            <h2 class="timeline-date">19</h2>
                         </div>
 
                             <div class="program-line"></div>
                         <div class= "program-text">
-                        <h2 class="program-title">Microloan Repayment</h2>
-                        <p class="program-desc">Don't forget to pay your microloans! The full amount is $100 <br />loan + $50 program fee!</p>    
+                        <h2 class="program-title">Slide Deck + Business Plan</h2>
+                        <p class="program-desc">Final deadline to submit your slide deck and business plan! Get practicing for final comp!</p>    
    
                         </div>
                         </div>
 
                     <div class="program" id="businessPlan">
                         <div class="program-date">
-                            <h2 class="timeline-month"> Jan</h2>
-                            <h2 class="timeline-date">20</h2>
-                        </div>
-
-                        <div class="program-line"></div>
-                        <div class= "program-text">
-        
-                        <h2 class="program-title">Business Plan Submission</h2>
-                        <p class="program-desc"> Please submit your final competition slides and your <br /> business plans in PDF form under the "Final Competition"<br /></p>
-   
-                        </div>
-                        </div>
-                    
-                    
-
-                    <div class="program" id="finComp">
-                        <div class="program-date">
-                            <h2 class="timeline-month"> Jan</h2>
+                            <h2 class="timeline-month">May</h2>
                             <h2 class="timeline-date">21</h2>
                         </div>
 
                         <div class="program-line"></div>
                         <div class= "program-text">
         
-                        <h2 class="program-title">Final Competition Day! </h2>
-                        <p class="program-desc">Final competition day! Get ready to present, business <br /> casual and monitor the "Final Competition" tab</p>
+                        <h2 class="program-title">Final Competition</h2>
+                        <p class="program-desc">D-DAY for Final Competition. See you all at Sauder!</p>
    
                         </div>
-                    </div>
+                        </div>
+                    
+                
                 </div>
 
                 
